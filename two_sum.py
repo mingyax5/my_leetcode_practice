c8bcdd8abf80c4bba7755743cba9bfc2cad7d09f
Mingyan Xu <xumingyan@xumingyandeMacBook-Air.local>
2024-06-30 15:56:09 +0800

    commit two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,11 @@
+
+
+
+def find_two_sum(nums, target):
+    """
+    :param nums: list of ints 整数数组
+    :param target: int 两数之和
+    :return: list of two ints 两个数的index
+    """
+
+    for index, num in enumerate(nums):
