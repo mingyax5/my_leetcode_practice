066324124182c148fcd25958f1845bbecfc4403e
Mingyan Xu <mingyax5@outlook.com>
2024-07-02 02:45:54 +0800

    add roman_int

---
+++ roman_int.py
@@ -0,0 +1,20 @@
+
+def romanToInt(s:str) -> int:
+    dict = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
+    n = len(s)
+    sum = 0
+
+    for i in range(n - 1):
+        if dict[s[i]] < dict[s[i+1]]:
+            sum -= dict[s[i]]
+        else:
+            sum += dict[s[i]]
+    return sum + dict[s[-1]]
