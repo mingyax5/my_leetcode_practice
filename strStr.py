f9d0a345044d6457f717331a2cdf027ef985c3da
Mingyan Xu <mingyax5@outlook.com>
2024-07-04 21:29:45 +0800

    commit strStr.py

---
+++ strStr.py
@@ -0,0 +1,18 @@
+
+def strStr(haystack: str, needle: str) -> int:
+    if len(haystack) == 1:
+        if haystack == needle:
+            x = 0
+        else: 
+            x = -1
+    else:
+        for i in range(0,len(haystack) - 1,1):
+            if needle == haystack[i:i+len(needle)]:
+                x = i
+                break
+            if needle == haystack[-1]:
+                x = len(haystack) - 1
+                break
+            else:
+                x = -1
+    return x
